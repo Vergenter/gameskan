5ae238d52632432081824c69d336dd06fb94a928
Oskar-Kosciolek <osasuna51@gmail.com>
2022-01-20 19:03:46 +0100

    Add base tiles to utils

--- utils.py
+++ utils.py
@@ -16,7 +16,9 @@ def load_test_assets():
         'altars': ['dung_altar.1.png', 'dung_altar.2.png', 'dung_altar.3.png', 'dung_altar_used.png'],
         'ladder': ['dung_ladderdown.png'],
         'monsters': ['mon_bomb.png', 'mon_bow.png', 'mon_wizard.png', 'mon_bomb_cd.png', 'mon_sword.png', 'mon_wizard_cd.png'],
-        'player': ['player_fleece.png', 'player_nospear.png', 'player_nospear_fleece.png']
+        'player': ['player_fleece.png', 'player_nospear.png', 'player_nospear_fleece.png'],
+        'floors': ['tile_base.png', 'tile_floor.1.png', 'tile_floor.2.png', 'tile_floor_large.1.png', 'tile_floor_large.2.png'],
+        'liquids': ['tile_liquid.1.png', 'tile_liquid.2.png', 'tile_liquid.3.png', 'tile_liquid_large.1.png', 'tile_liquid_large.2.png', 'tile_liquid_large.3.png'],
     }
 
     assets_loaded = {}
@@ -28,6 +30,8 @@ def load_test_assets():
 
 def bgra2rgba(img):
     return cv.cvtColor(img, cv.COLOR_BGRA2RGBA)
+
+
 
 
 def resize_image(img, scale: int, interpolation: Optional[int] = cv.INTER_AREA):
